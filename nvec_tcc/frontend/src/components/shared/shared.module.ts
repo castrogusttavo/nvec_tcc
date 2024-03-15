0fadc71ce81c2a75468fbbd066bc0fd3de68d7fb
JenGama <gamajennifer49@gmail.com>
2024-03-14 22:41:33 -0300

    remove card-listas and card-listas-laranja folders

--- nvec_tcc/frontend/src/components/shared/shared.module.ts
+++ nvec_tcc/frontend/src/components/shared/shared.module.ts
@@ -6,49 +6,44 @@ import { ButtonMediumOutlineComponent } from './buttons/button-medium-outline/bu
 import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { CardItemComponent } from './cards/card-item/card-item.component';
-import { CardListaLaranjaComponent } from './cards/card-listas-laranja/card-lista-laranja.component';
-import { CardListasComponent } from './cards/card-listas-azul/card-listas.component';
 import { CardIconHomeComponent } from './cards/card-icon-home/card-icon-home.component';
 import { InputComponent } from './inputs/input/input.component';
 import { CardBannerComponent } from './cards/card-banner/card-banner.component';
-
+import { CardListComponent } from './cards/card-list/card-list.component';
 
 @NgModule({
   declarations: [
     ButtonBigComponent,
     ButtonMediumComponent,
     ButtonMediumOutlineComponent,
-    CardListasComponent,
-    CardListaLaranjaComponent,
     CardItemComponent,
     CardIconHomeComponent,
     InputComponent,
     CardBannerComponent,
-    CardIconHomeComponent
+    CardIconHomeComponent,
+    CardListComponent
 
   ],
   imports: [
     IonicModule,
     CommonModule,
-    RouterModule.forChild([{path: '../../../components/shared/cards/card-listas-azul', component: CardListasComponent}]),
-    RouterModule.forChild([{path: '../../../components/shared/cards/card-listas-laranja', component: CardListaLaranjaComponent}]),
     RouterModule.forChild([{path: '../../../components/shared/cards/card-item', component: CardItemComponent}]),
     RouterModule.forChild([{path: '../../../components/shared/cards/card-icon-home', component: CardIconHomeComponent}]),
-    RouterModule.forChild([{path: '../../../components/shared/cards/card-banner', component: CardBannerComponent}])
+    RouterModule.forChild([{path: '../../../components/shared/cards/card-banner', component: CardBannerComponent}]),
+    RouterModule.forChild([{path: '../../../components/shared/cards/card-list', component: CardListComponent}])
     
   ],
   exports: [
     ButtonBigComponent,
     ButtonMediumComponent,
     ButtonMediumOutlineComponent,
-    CardListasComponent,
     CommonModule,
-    CardListaLaranjaComponent,
     CardItemComponent, // Também exporte o CommonModule se você estiver usando diretivas do Angular como *ngFor ou *ngIf em seus componentes compartilhados
     CardIconHomeComponent,
     InputComponent,
     CardBannerComponent,
-    CardIconHomeComponent
+    CardIconHomeComponent,
+    CardListComponent
   ]
 })
 export class SharedModule { }
