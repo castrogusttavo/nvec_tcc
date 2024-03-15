06d92b06dd83bd79a8f7c53d8a1aef7f1a8a8407
JenGama <gamajennifer49@gmail.com>
2024-03-14 22:19:08 -0300

    exclude card-banner-yellow and card-banner-blue folders

--- nvec_tcc/frontend/src/components/shared/shared.module.ts
+++ nvec_tcc/frontend/src/components/shared/shared.module.ts
@@ -5,8 +5,6 @@ import { ButtonMediumComponent } from './buttons/button-medium/button-medium.com
 import { ButtonMediumOutlineComponent } from './buttons/button-medium-outline/button-medium-outline.component';
 import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-import { CardBannerBlueComponent } from './cards/card-banner-blue/card-banner-blue.component';
-import { CardBannerYellowComponent } from './cards/card-banner-yellow/card-banner-yellow.component';
 import { CardItemComponent } from './cards/card-item/card-item.component';
 import { CardListaLaranjaComponent } from './cards/card-listas-laranja/card-lista-laranja.component';
 import { CardListasComponent } from './cards/card-listas-azul/card-listas.component';
@@ -22,8 +20,6 @@ import { CardBannerComponent } from './cards/card-banner/card-banner.component';
     CardListasComponent,
     CardListaLaranjaComponent,
     CardItemComponent,
-    CardBannerYellowComponent,
-    CardBannerBlueComponent,
     CardIconHomeComponent,
     InputComponent,
     CardBannerComponent
@@ -34,8 +30,6 @@ import { CardBannerComponent } from './cards/card-banner/card-banner.component';
     RouterModule.forChild([{path: '../../../components/shared/cards/card-listas-azul', component: CardListasComponent}]),
     RouterModule.forChild([{path: '../../../components/shared/cards/card-listas-laranja', component: CardListaLaranjaComponent}]),
     RouterModule.forChild([{path: '../../../components/shared/cards/card-item', component: CardItemComponent}]),
-    RouterModule.forChild([{path: '../../../components/shared/cards/card-banner-yellow', component: CardBannerYellowComponent}]),
-    RouterModule.forChild([{path: '../../../components/shared/cards/card-banner-blue', component: CardBannerBlueComponent}]),
     RouterModule.forChild([{path: '../../../components/shared/cards/card-icon-home', component: CardIconHomeComponent}]),
     RouterModule.forChild([{path: '../../../components/shared/cards/card-banner', component: CardBannerComponent}])
     
@@ -47,9 +41,7 @@ import { CardBannerComponent } from './cards/card-banner/card-banner.component';
     CardListasComponent,
     CommonModule,
     CardListaLaranjaComponent,
-    CardItemComponent,
-    CardBannerYellowComponent,
-    CardBannerBlueComponent, // Também exporte o CommonModule se você estiver usando diretivas do Angular como *ngFor ou *ngIf em seus componentes compartilhados
+    CardItemComponent, // Também exporte o CommonModule se você estiver usando diretivas do Angular como *ngFor ou *ngIf em seus componentes compartilhados
     CardIconHomeComponent,
     InputComponent,
     CardBannerComponent
