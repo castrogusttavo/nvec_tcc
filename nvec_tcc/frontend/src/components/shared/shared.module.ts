a98dfe92195bc8a2eb0cf041f506814fa93dc873
JenGama <gamajennifer49@gmail.com>
2024-03-14 22:30:02 -0300

    solve files in conflict

--- nvec_tcc/frontend/src/components/shared/shared.module.ts
+++ nvec_tcc/frontend/src/components/shared/shared.module.ts
@@ -5,18 +5,13 @@ import { ButtonMediumComponent } from './buttons/button-medium/button-medium.com
 import { ButtonMediumOutlineComponent } from './buttons/button-medium-outline/button-medium-outline.component';
 import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
-import { CardBannerBlueComponent } from './cards/card-banner-blue/card-banner-blue.component';
-import { CardBannerYellowComponent } from './cards/card-banner-yellow/card-banner-yellow.component';
 import { CardItemComponent } from './cards/card-item/card-item.component';
 import { CardListaLaranjaComponent } from './cards/card-listas-laranja/card-lista-laranja.component';
 import { CardListasComponent } from './cards/card-listas-azul/card-listas.component';
 import { CardIconHomeComponent } from './cards/card-icon-home/card-icon-home.component';
-<<<<<<< Updated upstream
 import { InputComponent } from './inputs/input/input.component';
 import { CardBannerComponent } from './cards/card-banner/card-banner.component';
-=======
 
->>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
@@ -26,16 +21,11 @@ import { CardBannerComponent } from './cards/card-banner/card-banner.component';
     CardListasComponent,
     CardListaLaranjaComponent,
     CardItemComponent,
-    CardBannerYellowComponent,
-    CardBannerBlueComponent,
-<<<<<<< Updated upstream
     CardIconHomeComponent,
     InputComponent,
-    CardBannerComponent
-=======
+    CardBannerComponent,
     CardIconHomeComponent
 
->>>>>>> Stashed changes
   ],
   imports: [
     IonicModule,
@@ -43,8 +33,6 @@ import { CardBannerComponent } from './cards/card-banner/card-banner.component';
     RouterModule.forChild([{path: '../../../components/shared/cards/card-listas-azul', component: CardListasComponent}]),
     RouterModule.forChild([{path: '../../../components/shared/cards/card-listas-laranja', component: CardListaLaranjaComponent}]),
     RouterModule.forChild([{path: '../../../components/shared/cards/card-item', component: CardItemComponent}]),
-    RouterModule.forChild([{path: '../../../components/shared/cards/card-banner-yellow', component: CardBannerYellowComponent}]),
-    RouterModule.forChild([{path: '../../../components/shared/cards/card-banner-blue', component: CardBannerBlueComponent}]),
     RouterModule.forChild([{path: '../../../components/shared/cards/card-icon-home', component: CardIconHomeComponent}]),
     RouterModule.forChild([{path: '../../../components/shared/cards/card-banner', component: CardBannerComponent}])
     
@@ -56,16 +44,11 @@ import { CardBannerComponent } from './cards/card-banner/card-banner.component';
     CardListasComponent,
     CommonModule,
     CardListaLaranjaComponent,
-    CardItemComponent,
-    CardBannerYellowComponent,
-    CardBannerBlueComponent, // Também exporte o CommonModule se você estiver usando diretivas do Angular como *ngFor ou *ngIf em seus componentes compartilhados
-<<<<<<< Updated upstream
+    CardItemComponent, // Também exporte o CommonModule se você estiver usando diretivas do Angular como *ngFor ou *ngIf em seus componentes compartilhados
     CardIconHomeComponent,
     InputComponent,
-    CardBannerComponent
-=======
+    CardBannerComponent,
     CardIconHomeComponent
->>>>>>> Stashed changes
   ]
 })
 export class SharedModule { }
