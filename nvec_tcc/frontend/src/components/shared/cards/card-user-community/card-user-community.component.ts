cb28cf485387af93f791894c58f069889a5a157d
JenGama <gamajennifer49@gmail.com>
2024-03-19 19:30:35 -0300

    create card-user-community

---
+++ nvec_tcc/frontend/src/components/shared/cards/card-user-community/card-user-community.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-user-community',
+  templateUrl: './card-user-community.component.html',
+  styleUrls: ['./card-user-community.component.scss'],
+})
+export class CardUserCommunityComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
