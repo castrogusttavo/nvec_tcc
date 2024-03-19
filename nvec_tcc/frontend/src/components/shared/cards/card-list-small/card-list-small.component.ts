d07f08eee87251d16ef6837b5e5c68aa8fef4904
Jennifer Santos Sousa Gama <100050649+JenGama@users.noreply.github.com>
2024-03-19 17:32:16 -0300

    create component card-list-small

---
+++ nvec_tcc/frontend/src/components/shared/cards/card-list-small/card-list-small.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-list-small',
+  templateUrl: './card-list-small.component.html',
+  styleUrls: ['./card-list-small.component.scss'],
+})
+export class CardListSmallComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
