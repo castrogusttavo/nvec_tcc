165a4c034b4d6a943934944d4b3c4cf6d75cd2ad
Gabrielly-Souza123 <gabriellysouza01123@gmail.com>
2024-03-19 22:52:35 -0300

    criação de multi telas
    muitas telas feitas

---
+++ nvec_tcc/frontend/src/app/load-screen3/load-screen3.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-load-screen3',
+  templateUrl: './load-screen3.page.html',
+  styleUrls: ['./load-screen3.page.scss'],
+})
+export class LoadScreen3Page implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
