ebe4b2a8382dccf8b4930e19e0896a41eaf3a798
roArthur <sofia_srr@outlook.com.br>
2024-03-25 20:47:01 -0300

    feature-card-plans
    criei um campo dentro de um component que tem as tags variaveis

--- frontend/src/components/shared/cards/card-plans/card-plans.component.ts
+++ frontend/src/components/shared/cards/card-plans/card-plans.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-plans',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-plans.component.scss'],
 })
 export class CardPlansComponent  implements OnInit {
+
+  @Input() planItems: string[] | undefined;
 
   constructor() { }
 
