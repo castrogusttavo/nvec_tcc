b1859ad824049ba85e195fbb08328054c33e09ea
Marife1 <128438635+castrogusttavo@users.noreply.github.com>
2024-03-25 20:47:48 -0300

    update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,20 +4,15 @@ const db = require("./db");
 
 // Importando o arquivo de rotas das APIs
 const userRouter = require("./api/users/users");
+app.use('/api', userRouter); 
 
-// Criando uma instância do express
 const app = express();
-
-// configurando o body-parser para analisar corpos de solicitações JSON
 app.use(bodyParser.json());
 
 /* Definindo um endpoint
     - req = request -> pedido ao servidor
     - res = response -> resposta do servidor
 */
-app.use('/api', userRouter); 
-
-// Defina uma rota para a raiz do servidor
 app.get("/", (req, res) => {
   res.send("Servidor rodando!");
 });
