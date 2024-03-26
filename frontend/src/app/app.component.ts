0014d00c2e34c7f11df65f4b9635c6eb01ba63fc
Gusttavo Castro <gusttavoleandro2023@gmail.com>
2024-03-26 02:01:39 -0300

    update splash-screen

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private splashScreen: SplashScreenPage) {
+    this.initialization();
+  }
+
+  initializeApp() {
+    this.splashScreen.show();
+    // Lógica de inicialização do aplicativo
+    // Por exemplo, carregar dados, inicializar plugins, etc.
+    // Depois que tudo estiver pronto, você pode esconder a splash-screen
+    setTimeout(() => {
+      this.splashScreen.hide();
+    }, 3000); // Tempo em milissegundos para exibir a splash-screen
+  }
 }
