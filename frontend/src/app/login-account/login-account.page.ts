5a0478d96323a0fb6be8edd6802d7411b96f838f
castrogusttavo <128438635+castrogusttavo@users.noreply.github.com>
2024-04-02 22:36:17 -0300

    bug login backend

--- frontend/src/app/login-account/login-account.page.ts
+++ frontend/src/app/login-account/login-account.page.ts
@@ -1,23 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
+import { AuthService } from '../../../../backend/src/services/auth.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login-account',
   templateUrl: './login-account.page.html',
   styleUrls: ['./login-account.page.scss'],
 })
+
 export class LoginAccountPage implements OnInit {
   email: string = '';
   senha: string = '';
 
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   fazerLogin() {
-  
-  }
+    const dadosLogin ={
+      email: this.email,
+      senha: this.senha
+    };
 
+    this.authService.login(dadosLogin).subscribe(() =>{
+      console.log('Login feito com sucesso');
+    },
+    (err: any) => {
+      console.error('Erro ao fazer login:', err)
+    });
+  }
 }
