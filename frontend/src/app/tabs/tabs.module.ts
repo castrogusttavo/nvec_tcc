9344f6d3e0db94ca0d3038c23d4c0c941ef24232
Sofia Rodrigues Rocha <sofia_srr@outlook.com.br>
2024-04-09 21:15:01 -0300

    modal

--- frontend/src/app/tabs/tabs.module.ts
+++ frontend/src/app/tabs/tabs.module.ts
@@ -6,13 +6,16 @@ import { FormsModule } from '@angular/forms';
 import { TabsPageRoutingModule } from './tabs-routing.module';
 
 import { TabsPage } from './tabs.page';
+import { SharedModule } from 'src/components/shared/shared.module';
+
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    TabsPageRoutingModule
+    TabsPageRoutingModule,
+    SharedModule
   ],
   declarations: [TabsPage]
 })
