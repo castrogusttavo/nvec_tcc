27974cdeecc5ae441fa6b6c004fa0f89606798d8
Sofia Rodrigues Rocha <sofia_srr@outlook.com.br>
2024-04-28 23:44:08 -0300

    finish-inputs
    Terminei os inputs, só vai faltar uns detalhes de css a ser discutido, passar pro modal e os formgroup

--- frontend/src/app/tab3/tab3.module.ts
+++ frontend/src/app/tab3/tab3.module.ts
@@ -6,7 +6,8 @@ import { Tab3Page } from './tab3.page';
 import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 import { SharedModule } from 'src/components/shared/shared.module';
 import { Tab3PageRoutingModule } from './tab3-routing.module';
-
+import { ExampleComponent } from 'src/components/shared/modals/example/example.component';
+import { TypeaheadComponent } from 'src/components/shared/modals/typeahead/typeahead.component';
 
 @NgModule({
   imports: [
@@ -17,6 +18,6 @@ import { Tab3PageRoutingModule } from './tab3-routing.module';
     Tab3PageRoutingModule,
     SharedModule,
   ],
-  declarations: [Tab3Page]
+  declarations: [Tab3Page, ExampleComponent, TypeaheadComponent]
 })
 export class Tab3PageModule {}
