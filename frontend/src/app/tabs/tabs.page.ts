7f3a54d08c9248830ff4708e3e7b80340f89a3d4
TufiaDev <130183994+TufiaDev@users.noreply.github.com>
2024-05-02 22:48:52 -0300

    feature-modal
    Quase terminando, falta tutoriais e detalhes

--- frontend/src/app/tabs/tabs.page.ts
+++ frontend/src/app/tabs/tabs.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tabs',
@@ -7,11 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TabsPage {
   isModalOpen = false;
-
   setOpen(isOpen: boolean) {
     this.isModalOpen = isOpen;
   }
   
-  constructor() {}
+    // FormGroup para validação dos campos de texto
+  textForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    // Inicialização do FormGroup para validação dos campos de texto
+    this.textForm = this.formBuilder.group({
+      valorMaximo: ['', [Validators.required, Validators.pattern(/^\d+(\.\d{1,2})?$/)]],
+      valor: ['', [Validators.required, Validators.pattern(/^\d+(\.\d{1,2})?$/)]],
+      peso: ['', [Validators.required, Validators.pattern(/^\d+(\.\d{1,2})?$/)]],
+    });
+  }
 
 }
