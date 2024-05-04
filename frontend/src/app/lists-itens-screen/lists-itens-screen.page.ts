c6a44083949bbd9e18e1f38438e3b691a7e2dd88
Gusttavo Castro <gusttavoleandro2023@gmail.com>
2024-05-04 07:09:05 -0300

    update project

--- frontend/src/app/lists-itens-screen/lists-itens-screen.page.ts
+++ frontend/src/app/lists-itens-screen/lists-itens-screen.page.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-lists-itens-screen',
@@ -7,24 +6,19 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./lists-itens-screen.page.scss'],
 })
 export class ListsItensScreenPage implements OnInit {
-  isModalOpen = false;
-  setOpen(isOpen: boolean) {
-    this.isModalOpen = isOpen;
+  isModalOpen1 = false;
+  isModalOpen2 = false;
+
+  setOpen(modalNumber: number, isOpen: boolean) {
+    if (modalNumber === 1) {
+      this.isModalOpen1 = isOpen;
+    } else if (modalNumber === 2) {
+      this.isModalOpen2 = isOpen;
+    }
   }
 
-  // FormGroup para validação dos campos de texto
-  textForm: FormGroup;
-
-  constructor(private formBuilder: FormBuilder) {
-    // Inicialização do FormGroup para validação dos campos de texto
-    this.textForm = this.formBuilder.group({
-      valorMaximo: ['', [Validators.required, Validators.pattern(/^\d+(\.\d{1,2})?$/)]],
-      valor: ['', [Validators.required, Validators.pattern(/^\d+(\.\d{1,2})?$/)]],
-      peso: ['', [Validators.required, Validators.pattern(/^\d+(\.\d{1,2})?$/)]],
-    });
-  }
+  constructor() { }
 
   ngOnInit() {
   }
-
 }
