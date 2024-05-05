fbadd184ef3896a2d79648d824f7628bea030c2d
Gabrielly Machado <132954567+Gabrielly-Souza123@users.noreply.github.com>
2024-05-05 16:44:54 -0300

    criação da page de relatorio

--- frontend/src/app/report/report.page.ts
+++ frontend/src/app/report/report.page.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class ReportPage implements OnInit {
-  segment: string = 'segment';
-  secondary: string = 'secondary';
+  public segmentValue: string = 'segment1'; 
   constructor() { }
+
+  segmentChanged(event: any){
+    this.segmentValue = event.detail.value;
+  }
 
   ngOnInit() {
     
