e3941e823ded8f405ef288a234521100df3e45b4
Sofia Rodrigues Rocha <sofia_srr@outlook.com.br>
2024-05-07 15:41:03 -0300

    feature-search-bar

--- frontend/src/app/tab4/tab4.page.ts
+++ frontend/src/app/tab4/tab4.page.ts
@@ -6,7 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab4.page.scss'],
 })
 export class Tab4Page implements OnInit {
+  searchText: string = '';
+  originalItems: any[] = [
+    { title: 'Criar comunidade', imagePath: './../assets/svg/add.svg' },
+    { title: 'Volta às Aulas', imagePath: 'https://blog.etiquetaseadesivos.com.br/wp-content/uploads/2021/12/various-stationery-school-and-office-supplies-over-wooden-texture-picture-id546761524.jpg' },
+    { title: 'Atacadista', imagePath: 'https://newtrade.com.br/wp-content/uploads/2017/05/supermercado-10-05.jpg' }
+  ];
+  itemsToShow: any[] = this.originalItems;
+  
+  onSearchInput(event: any) {
+    this.searchText = event.target.value;
+    this.filterItems();
+  }
 
+  filterItems() {
+    if (this.searchText === '') {
+      this.itemsToShow = this.originalItems;
+      return;
+    }
+
+    this.itemsToShow = this.originalItems.filter(item => 
+      item.title.toLowerCase().includes(this.searchText.toLowerCase())
+    );
+  }
   constructor() { }
 
   ngOnInit() {
