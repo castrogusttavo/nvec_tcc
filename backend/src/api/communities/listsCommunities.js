00e7a43a217f8979bea709f5a4e9824ed38c5789
Gusttavo Castro <gusttavoleandro2023@gmail.com>
2024-05-22 21:33:33 -0300

    cli: Rabisco das Próximas APIs

---
+++ backend/src/api/communities/listsCommunities.js
@@ -0,0 +1,3 @@
+// JOIN para pegar todas as listas da comunidade
+  // Tabela comunidade_listas -> listas usuários
+  // Nova Tabela lista_base_comunidade -> lista base com os itens
---
+++ backend/src/api/report/report.js
@@ -0,0 +1,12 @@
+// Total Gasto
+  // vl_gasto por lista + id_ctg 
+  // 4 categorias que mais gastou
+
+// Total Economizado
+  // vl_total - vl_gasto + id_ctg
+  // 4 categorias que mais economizou
+
+// Balanço geral
+  // Listas Criadas pelo Usuário
+  // Comparações feitas
+  // Listas Finalizadas
