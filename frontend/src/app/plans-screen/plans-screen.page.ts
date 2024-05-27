e2f738dd41b7d019a3af8ee0042625108ceb17e2
Gusttavo Castro <gusttavoleandro2023@gmail.com>
2024-05-26 23:14:35 -0300

    cli: Add possibility for user to change Plan

--- frontend/src/app/plans-screen/plans-screen.page.ts
+++ frontend/src/app/plans-screen/plans-screen.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-plans-screen',
@@ -9,7 +10,7 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 export class PlansScreenPage implements OnInit {
   userSubscriptionId: number | undefined;
 
-  constructor(private jwtHelper: JwtHelperService) {}
+  constructor(private jwtHelper: JwtHelperService, private http: HttpClient) {}
 
   ngOnInit() {
     this.getUserSubscriptionId();
@@ -25,5 +26,23 @@ export class PlansScreenPage implements OnInit {
 
   isPlanActive(planId: number): boolean {
     return this.userSubscriptionId === planId;
+  }
+
+  updateUserSubscription(planId: number): void {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const decodedToken = this.jwtHelper.decodeToken(token);
+      const userId = decodedToken.userId;
+      this.http.patch<any>(`http://localhost:3001/api/users/${userId}`, { id_assinatura: planId }).subscribe(
+        (data) => {
+          console.log('ID de assinatura do usuário atualizado com sucesso:', data);
+          this.userSubscriptionId = planId; // Atualiza o ID de assinatura do usuário localmente
+        },
+        (error) => {
+          console.error('Erro ao atualizar ID de assinatura do usuário:', error);
+          // Tratar erros ou exibir uma mensagem de erro para o usuário
+        }
+      );
+    }
   }
 }
