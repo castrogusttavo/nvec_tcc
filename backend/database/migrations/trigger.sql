2e8539b4d0068a8e5d4dfa1166bfd6c36829c251
Gusttavo Castro <128438635+castrogusttavo@users.noreply.github.com>
2024-06-14 20:02:14 -0300

    cli: Fix Triggers and Update Data.sql

--- backend/database/migrations/trigger.sql
+++ backend/database/migrations/trigger.sql
@@ -1,27 +1,26 @@
+-- trigger para definir a data de criação na tabela tb_comunidade
 delimiter $
 create trigger trg_insert_comunidade 
-	before insert on tb_comunidade
-		for each row
-			begin
-				set new.dt_criacao=current_date();
-			end;
+before insert on tb_comunidade
+for each row
+begin
+    set new.dt_criacao = current_date();
 end;
- $
+$
 delimiter ;
 
-
+-- trigger para definir a data de criação na tabela tb_lista
 delimiter $
 create trigger trg_insert_lista 
-	before insert on tb_lista
-		for each row
-			begin
-				set new.dt_criacao=current_date();
-			end;
+before insert on tb_lista
+for each row
+begin
+    set new.dt_criacao = current_date();
 end;
- $ 
+$
 delimiter ;
 
-
+-- trigger para inserir na tabela tb_lista_variavel após inserção em tb_comunidade_usuario
 delimiter $
 create trigger insert_comunidade_usuario
 after insert on tb_comunidade_usuario
@@ -29,15 +28,17 @@ for each row
 begin
     declare lista_fixa_exists int;
 
-    select count(*) into lista_fixa_exists
+    select count(*)
+    into lista_fixa_exists
     from tb_lista_fixa 
-    where id_lista_fixa = NEW.id_comunidade;
+    where id_comunidade = new.id_comunidade;
 
     if lista_fixa_exists > 0 then
         insert into tb_lista_variavel (id_usuario, id_lista_fixa, end_lista)
-        values (NEW.id_usuario, NEW.id_comunidade, '');
+        values (new.id_usuario, 
+               (select id_lista_fixa from tb_lista_fixa where id_comunidade = new.id_comunidade limit 1), 
+               '');
     end if;
 end;
 $
 delimiter ;
-
--- backend/database/seeds/data.sql
+++ backend/database/seeds/data.sql
@@ -1,51 +1,50 @@
 -- Preencher a tabela de usuários
 INSERT INTO tb_usuario (nm_usuario, senha_usuario, email_usuario, id_assinatura) VALUES
-    ('Gusttavo Castro', '$2b$10$6HZ9emoZIJPb1O.0x91pp.MKiVtmA6fyIDTraZ3WT4Hvp0/SiWN.S', 'castro@gmail.com', 1),
-    ('Jennifer Gama', '$2b$10$6HZ9emoZIJPb1O.0x91pp.MKiVtmA6fyIDTraZ3WT4Hvp0/SiWN.S', 'gama@gmail.com', 2);
+    ('Gusttavo Castro', '$2b$10$MhOpRFnAGFHNAVZS523kp.IULBZ32FBV/F6kofdxMQ3xE2Qn9rsHS', 'castro@gmail.com', 1),
+    ('Jennifer Gama', '$2b$10$MhOpRFnAGFHNAVZS523kp.IULBZ32FBV/F6kofdxMQ3xE2Qn9rsHS', 'gama@gmail.com', 2);
 
 -- Preencher a tabela de comunidades
-INSERT INTO tb_comunidade (nm_comunidade, sb_comunidade, end_comunidade, id_categoria) VALUES
-    ('Comunidade de Tecnologia', 'Discussão sobre tecnologias emergentes', 'Endereco Tecnologico', 9),
-    ('Comunidade de Literatura', 'Discussão sobre livros e literatura', 'Endereco Literario', 13);
+INSERT INTO tb_comunidade (nm_comunidade, sb_comunidade, end_comunidade, dt_criacao, id_categoria, id_criador) VALUES
+    ('Comunidade de Tecnologia', 'Discussão sobre tecnologias emergentes', 'Endereco Tecnologico', '2024-01-01', 9, 1),
+    ('Comunidade de Literatura', 'Discussão sobre livros e literatura', 'Endereco Literario', '2024-01-01', 13, 2);
 
 -- Preencher a tabela de listas
 INSERT INTO tb_lista (nm_lista, dt_criacao, ds_lista, rd_lista, end_lista, id_categoria, id_usuario) VALUES
     ('Compras de Supermercado', '2024-05-01', 'Lista de compras para o mês', 100.00, 'Endereco Supermercado', 17, 1),
-    ('Materiais de Escritório', '2024-06-02', 'Lista de materiais para o escritório', 50.00, 'Endereco Escritorio', 14, 1);
+    ('Materiais de Escritório', '2024-06-02', 'Lista de materiais para o escritório', 50.00, 'Endereco Escritorio', 14, 1),
+    ('Lista de Eletrônicos', '2024-06-03', 'Lista de compras de eletrônicos', 500.00, 'Endereco Eletrônicos', 9, 1),
+    ('Lista de Roupas', '2024-05-04', 'Lista de compras de roupas', 200.00, 'Endereco Roupas', 16, 1),
+    ('Lista de Limpeza', '2024-05-05', 'Lista de compras de produtos de limpeza', 80.00, 'Endereco Limpeza', 15, 1);
 
 -- Preencher a tabela de itens
 INSERT INTO tb_item (nm_item, vl_uni, qtde_item, qtde_medida_item, id_status, id_medida, id_lista) VALUES
     ('Maçã', 20.00, 2, 1, 1, 1, 1),
-    ('Feijão', 5.00, 3, 1, 2, 2, 1);
-    
+    ('Feijão', 5.00, 3, 1, 2, 2, 1),
+    ('TV', 1000.00, 1, 1, 2, 1, 3),
+    ('Camisa', 50.00, 2, 1, 2, 1, 4),
+    ('Sabão em Pó', 10.00, 2, 1, 2, 1, 5);
+
 -- Preencher a tabela de listas fixas
-INSERT INTO tb_lista_fixa (id_item, qtde_item, id_medida, id_usuario) VALUES
-    ('1', 2, 1, 1),
-    ('2', 3, 2, 1);
+INSERT INTO tb_lista_fixa (id_comunidade) VALUES
+    (1),
+    (2);
 
--- Preencher a tabela de despesas
-INSERT INTO tb_lista_variavel (id_lista_fixa, vl_uni, id_usuario) VALUES
-    (1, 100.00, 1),
-    (2, 50.00, 1);
+-- Preencher a tabela de itens fixos
+INSERT INTO tb_item_fixo (id_lista_fixa, nm_item, qtde_medida, qtde_item, id_medida) VALUES
+    (1, 'Item Fixo 1', 2, 1, 1),
+    (2, 'Item Fixo 2', 3, 2, 2);
 
--- Preencher a tabela de comunidade-lista
-INSERT INTO tb_comunidade_lista (id_comunidade, id_lista_fixa) VALUES
-    (1, 1),
-    (2, 2);
+-- Preencher a tabela de listas variáveis
+INSERT INTO tb_lista_variavel (id_lista_fixa, id_usuario, end_lista) VALUES
+    (1, 1, 'Endereco 1'),
+    (2, 1, 'Endereco 2');
+
+-- Preencher a tabela de itens variáveis
+INSERT INTO tb_item_variavel (vl_uni, id_lista_variavel, id_item_fixo) VALUES
+    (100.00, 1, 1),
+    (50.00, 2, 2);
 
 -- Preencher a tabela de comunidade-usuário
 INSERT INTO tb_comunidade_usuario (id_comunidade, id_usuario) VALUES
     (1, 1),
     (2, 2);
-
--- Preencher a tabela de listas com mais dados
-INSERT INTO tb_lista (nm_lista, dt_criacao, ds_lista, rd_lista, end_lista, id_categoria, id_usuario) VALUES
-    ('Lista de Eletrônicos', '2024-06-03', 'Lista de compras de eletrônicos', 500.00, 'Endereco Eletrônicos', 9, 1),
-    ('Lista de Roupas', '2024-05-04', 'Lista de compras de roupas', 200.00, 'Endereco Roupas', 16, 1),
-    ('Lista de Limpeza', '2024-05-05', 'Lista de compras de produtos de limpeza', 80.00, 'Endereco Limpeza', 15, 1);
-
--- Preencher a tabela de itens com mais dados
-INSERT INTO tb_item (nm_item, vl_uni, qtde_item, qtde_medida_item, id_status, id_medida, id_lista) VALUES
-    ('TV', 1000.00, 1, 1, 2, 1, 3),
-    ('Camisa', 50.00, 2, 1, 2, 1, 4),
-    ('Sabão em Pó', 10.00, 2, 1, 2, 1, 5);
