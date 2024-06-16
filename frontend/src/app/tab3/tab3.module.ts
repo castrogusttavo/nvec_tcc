374625de2e209e694bca3f978546092c37a49649
Jennifer Santos Sousa Gama <100050649+JenGama@users.noreply.github.com>
2024-06-15 23:21:45 -0300

    delete pages

--- frontend/src/app/tab3/tab3.module.ts
+++ frontend/src/app/tab3/tab3.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
 import { SharedModule } from 'src/components/shared/shared.module';
 import { Tab3PageRoutingModule } from './tab3-routing.module';
 import { HttpClientModule } from '@angular/common/http';
@@ -18,7 +17,6 @@ import { ReactiveFormsModule } from '@angular/forms'; // Importe o ReactiveForms
     IonicModule,
     CommonModule,
     FormsModule,
-    ExploreContainerComponentModule,
     Tab3PageRoutingModule,
     SharedModule,
     ReactiveFormsModule ,
