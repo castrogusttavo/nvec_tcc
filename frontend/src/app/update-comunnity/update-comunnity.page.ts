77cfe4ea49c324955f08db506746a02a57bdf514
Gusttavo Castro <128438635+castrogusttavo@users.noreply.github.com>
2024-06-15 23:28:38 -0300

    fix: Placeholder Values

--- frontend/src/app/update-comunnity/update-comunnity.page.ts
+++ frontend/src/app/update-comunnity/update-comunnity.page.ts
@@ -1,8 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Observable, forkJoin, map } from 'rxjs';
+import { Observable } from 'rxjs';
 import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
@@ -12,38 +11,46 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 })
 export class UpdateComunnityPage implements OnInit {
 
-  userId!:number;
-  communityId!:number;
+  userId!: number;
+  communityId!: number;
   
-  name!:string;
-  category!:any[];
-  about!:string;
-  address!:string;
-  inputTextValue: string | undefined;
-  categoriaSelecionada!:string;
-  private apiCategories = 'http://localhost:3001/api/categories'
+  name: string = '';
+  oldName: string = '';
+  category: any[] = [];
+  about: string = '';
+  oldAbout: string = '';
+  address: string = '';
+  oldAddress: string = '';
+  oldCategoria: string = '';
 
-  constructor(private jwtHelper: JwtHelperService,private route: ActivatedRoute, private http: HttpClient, private router: Router,private formBuilder: FormBuilder) {}
+  categoriaSelecionada: string = '';
+
+  private apiCategories = 'http://localhost:3001/api/categories';
+  private apiCommunity = 'http://localhost:3001/api/communities';
+
+  constructor(
+    private jwtHelper: JwtHelperService,
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.userId = params['userId'];
       this.communityId = params['communityId'];
+      this.getCommunityDetails(this.communityId);
     });
-    this.getUserId();
     this.getCategories().subscribe(categories => {
       this.category = categories;
     });
-
   }
 
   async updateCommunity(event: { preventDefault: () => void; }) {
-
     event.preventDefault();
-
     try {
       const response: any = await this.http.patch(
-        `http://localhost:3001/api/communities/${this.communityId}`,
+        `${this.apiCommunity}/${this.communityId}`,
         { 
           nm_comunidade: this.name, 
           id_categoria: this.categoriaSelecionada, 
@@ -53,33 +60,31 @@ export class UpdateComunnityPage implements OnInit {
       ).toPromise();
 
       console.log('Comunidade atualizada com sucesso:', response);
-
-      this.router.navigate(['/tabs/tab4']);
+      this.router.navigate(['/list-adm-community', this.userId, this.communityId]);
     } catch (err) {
       console.error('Erro ao atualizar comunidade:', err);
     }
   }
 
-  getCategories():Observable<any[]>{
+  getCategories(): Observable<any[]> {
     return this.http.get<any[]>(this.apiCategories);
   }
 
-  getUserName(): void {
-    const token = localStorage.getItem('token');
-    console.log('Token:', token); 
-    if (token) {
-      const decodedToken = this.jwtHelper.decodeToken(token);
-      console.log('Decoded Token:', decodedToken.userId);
-      this.userId = decodedToken.userId;
-      console.log(this.userId);
-    }
+  getCommunityDetails(id: number): void {
+    this.http.get<any>(`${this.apiCommunity}/${id}`).subscribe(community => {
+      this.oldName = community.nm_comunidade;
+      this.oldAbout = community.sb_comunidade;
+      this.oldAddress = community.end_comunidade;
+      this.oldCategoria = community.ds_categoria; // Assuming ds_categoria is the display name of the category
+      this.categoriaSelecionada = community.id_categoria;
+      this.userId = community.id_usuario;
+    });
   }
 
   customCounterFormatter(inputLength: number, maxLength: number) {
     return inputLength > 0 ? `${inputLength} / ${maxLength}` : '';
   }
 
-  // Atualiza o contador de caracteres
   updateCounter(event: any) {
     const input = event.target;
     const maxLength = parseInt(input.getAttribute('maxlength'), 10);
@@ -90,16 +95,8 @@ export class UpdateComunnityPage implements OnInit {
     }
   }
 
-  getUserId(): void {
-    const token = localStorage.getItem('token');
-    if (token) {
-      const decodedToken = this.jwtHelper.decodeToken(token);
-      this.userId = decodedToken.userId;
-    }
-  }
-
   deleteCommunity(): void {
-    this.http.delete<any>(`http://localhost:3001/api/communities/${this.userId}/${this.communityId}`).subscribe(
+    this.http.delete<any>(`${this.apiCommunity}/${this.userId}/${this.communityId}`).subscribe(
       response => {
         console.log('Comnunidade excluída com sucesso:', response);
         this.router.navigate(['/tabs/tab4']);
@@ -109,7 +106,4 @@ export class UpdateComunnityPage implements OnInit {
       }
     );
   }
-
-
-} 
-
+}
