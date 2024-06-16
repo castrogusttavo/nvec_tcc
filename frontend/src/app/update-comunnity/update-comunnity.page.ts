54ef4fd43dc7774d7bee9bfdac287c7b91a774b7
Gusttavo Castro <128438635+castrogusttavo@users.noreply.github.com>
2024-06-15 23:02:22 -0300

    Revert "cli: Add Placeholder for OlderValue to Community, List and Item Update"
    This reverts commit a53fc146dc3568f61fc6d99cb7ba8ab2dd81a7d0.

--- frontend/src/app/update-comunnity/update-comunnity.page.ts
+++ frontend/src/app/update-comunnity/update-comunnity.page.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, forkJoin, map } from 'rxjs';
 import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Component({
@@ -11,42 +12,32 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 })
 export class UpdateComunnityPage implements OnInit {
 
-  userId!: number;
-  communityId!: number;
+  userId!:number;
+  communityId!:number;
   
-  name: string = '';
-  oldName: string = '';
-  category: any[] = [];
-  about: string = '';
-  oldAbout: string = '';
-  address: string = '';
-  oldAddress: string = '';
-  oldCategoria: string = '';
+  name!:string;
+  category!:any[];
+  about!:string;
+  address!:string;
+  inputTextValue: string | undefined;
+  categoriaSelecionada!:string;
+  private apiCategories = 'http://localhost:3001/api/categories'
 
-  categoriaSelecionada: string = '';
-
-  private apiCategories = 'http://localhost:3001/api/categories';
-  private apiCommunity = 'http://localhost:3001/api/communities';
-
-  constructor(
-    private jwtHelper: JwtHelperService,
-    private route: ActivatedRoute,
-    private http: HttpClient,
-    private router: Router
-  ) {}
+  constructor(private jwtHelper: JwtHelperService,private route: ActivatedRoute, private http: HttpClient, private router: Router,private formBuilder: FormBuilder) {}
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.userId = params['userId'];
       this.communityId = params['communityId'];
-      this.getCommunityDetails(this.communityId);
     });
     this.getCategories().subscribe(categories => {
       this.category = categories;
     });
+
   }
 
   async updateCommunity(event: { preventDefault: () => void; }) {
+
     event.preventDefault();
 
     console.log('Name: ', this.name);
@@ -57,41 +48,37 @@ export class UpdateComunnityPage implements OnInit {
 
     try {
       const response: any = await this.http.patch(
-        `${this.apiCommunity}/${this.communityId}`,
-        { 
-          nm_comunidade: this.name, 
-          id_categoria: this.categoriaSelecionada, 
-          sb_comunidade: this.about, 
-          end_comunidade: this.address 
-        }
+        `http://localhost:3001/api/communities/${this.communityId}`,
+        { nm_comunidade: this.name, id_categoria: this.categoriaSelecionada, sb_comunidade:this.about, end_comunidade:this.address}
       ).toPromise();
 
       console.log('Comunidade atualizada com sucesso:', response);
-      this.router.navigate(['/list-adm-community', this.userId, this.communityId]);
+      this.router.navigate(['/list-adm-community',this.userId, this.communityId]);
     } catch (err) {
       console.error('Erro ao atualizar comunidade:', err);
     }
   }
 
-  getCategories(): Observable<any[]> {
+  getCategories():Observable<any[]>{
     return this.http.get<any[]>(this.apiCategories);
   }
 
-  getCommunityDetails(id: number): void {
-    this.http.get<any>(`${this.apiCommunity}/${id}`).subscribe(community => {
-      this.oldName = community.nm_comunidade;
-      this.oldAbout = community.sb_comunidade;
-      this.oldAddress = community.end_comunidade;
-      this.oldCategoria = community.ds_categoria; // Assuming ds_categoria is the display name of the category
-      this.categoriaSelecionada = community.id_categoria;
-      this.userId = community.id_usuario;
-    });
+  getUserName(): void {
+    const token = localStorage.getItem('token');
+    console.log('Token:', token); 
+    if (token) {
+      const decodedToken = this.jwtHelper.decodeToken(token);
+      console.log('Decoded Token:', decodedToken.userId);
+      this.userId = decodedToken.userId;
+      console.log(this.userId);
+    }
   }
 
   customCounterFormatter(inputLength: number, maxLength: number) {
     return inputLength > 0 ? `${inputLength} / ${maxLength}` : '';
   }
 
+  // Atualiza o contador de caracteres
   updateCounter(event: any) {
     const input = event.target;
     const maxLength = parseInt(input.getAttribute('maxlength'), 10);
@@ -103,7 +90,7 @@ export class UpdateComunnityPage implements OnInit {
   }
 
   deleteCommunity(): void {
-    this.http.delete<any>(`${this.apiCommunity}/${this.userId}/${this.communityId}`).subscribe(
+    this.http.delete<any>(`http://localhost:3001/api/communities/${this.userId}/${this.communityId}`).subscribe(
       response => {
         console.log('Comnunidade excluída com sucesso:', response);
         this.router.navigate(['/tabs/tab4']);
@@ -113,4 +100,7 @@ export class UpdateComunnityPage implements OnInit {
       }
     );
   }
-}
+
+
+} 
+
--- frontend/src/app/update-list/update-list.page.ts
+++ frontend/src/app/update-list/update-list.page.ts
@@ -10,58 +10,35 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./update-list.page.scss'],
 })
 export class UpdateListPage implements OnInit {
-  name!: string;
-  oldName!: string;
-  categories: any[] = [];
-  description!: string;
-  oldDescription!: string;
-  expense!: string;
-  oldExpense!: string;
-  address!: string;
-  oldAddress!: string;
-  user!: string;
-  oldCategoria!: string;
+  name!:string;
+  category!:any[];
+  description!:string;
+  expense!:string;
+  address!:string;
+  user!:string;
 
-  categoriaSelecionada!: string;
+  inputTextValue: string | undefined;
+  categoriaSelecionada!:string;
+
   listaId!: string;
-
+ 
   private apiCategories = 'http://localhost:3001/api/categories';
   private apiList = 'http://localhost:3001/api/lists';
 
-  constructor(
-    private route: ActivatedRoute,
-    private http: HttpClient,
-    private router: Router,
-    private jwtHelper: JwtHelperService
-  ) { }
+  getCategories():Observable<any[]>{
+    return this.http.get<any[]>(this.apiCategories);
+  }
+
+  constructor(private route: ActivatedRoute,private http: HttpClient, private router: Router, private jwtHelper: JwtHelperService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.listaId = params['id'];
-      if (this.listaId) {
-        this.getListDetails(this.listaId);
-      }
     });
     this.getCategories().subscribe(categories => {
-      this.categories = categories;
+      this.category = categories;
     });
     this.getUserId();
-  }
-
-  getCategories(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiCategories);
-  }
-
-  getListDetails(id: string): void {
-    this.http.get<any>(`${this.apiList}/${id}`).subscribe(list => {
-      this.oldName = list.nm_lista;
-      this.oldDescription = list.ds_lista;
-      this.oldExpense = list.rd_lista;
-      this.oldAddress = list.end_lista;
-      this.oldCategoria = list.id_categoria;
-      this.categoriaSelecionada = list.id_categoria;
-      this.user = list.id_usuario;
-    });
   }
 
   customCounterFormatter(inputLength: number, maxLength: number) {
@@ -78,35 +55,29 @@ export class UpdateListPage implements OnInit {
     }
   }
 
+
   async updateList(event: { preventDefault: () => void; }) {
-    if (this.listaId) {
-      event.preventDefault();
+   if(this.listaId){
+    event.preventDefault();
 
-      console.log('Name:', this.name);
-      console.log('Categoria:', this.categoriaSelecionada);
-      console.log('Descrição:', this.description);
-      console.log('Endereço:', this.address);
-      console.log('Valor máximo:', this.expense);
+    console.log('Name:', this.name);
+    console.log('Categoria:', this.categoriaSelecionada);
+    console.log('Descrição:', this.description);
+    console.log('Endereço:', this.address);
+    console.log('Valor máximo:', this.expense);
 
-      try {
-        const response: any = await this.http.patch(
-          `${this.apiList}/${this.listaId}`,
-          {
-            nm_lista: this.name,
-            rd_lista: this.expense,
-            ds_lista: this.description,
-            id_categoria: this.categoriaSelecionada,
-            id_usuario: this.user,
-            end_lista: this.address
-          }
-        ).toPromise();
+    try {
+      const response: any = await this.http.patch(
+        `http://localhost:3001/api/lists/${this.listaId}`,
+        { nm_lista: this.name, rd_lista:this.expense, ds_lista:this.description,id_categoria:this.categoriaSelecionada, id_usuario:this.user, end_lista:this.address }
+      ).toPromise();
 
-        console.log('Lista atualizada com sucesso:', response);
-        this.router.navigate(['/tabs/tab1']);
-      } catch (err) {
-        console.error('Erro ao atualizar lista:', err);
-      }
+      console.log('Lista atualizada com sucesso:', response);
+      this.router.navigate(['/tabs/tab1']);
+    } catch (err) {
+      console.error('Erro ao atualizar lista:', err);
     }
+   }
   }
 
   getUserId(): void {
@@ -120,14 +91,17 @@ export class UpdateListPage implements OnInit {
   }
 
   deleteList(): void {
-    this.http.delete(`${this.apiList}/${this.listaId}`)
-      .toPromise()
-      .then(() => {
-        console.log('Lista excluída com sucesso');
-        this.router.navigate(['/tabs/tab1']);
-      })
-      .catch(error => {
-        console.error('Erro ao excluir lista:', error);
-      });
+
+    this.http.delete(`http://localhost:3001/api/lists/${this.listaId}`)
+    .toPromise()
+    .then(() => {
+      console.log('Lista excluída com sucesso');
+      this.router.navigate(['/tabs/tab1']);
+    })
+    .catch(error => {
+      console.error('Erro ao excluir lista:', error);
+
+    });
+
   }
 }
